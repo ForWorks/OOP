f4127eac7fb6bb6251db4bf08663e749ca26939c
ForWorks <vladikgatilo@gmail.com>
2021-04-03 18:16:53 +0300

    Third version

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -14,12 +15,12 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("main"), 800, 600);
+        stage.getIcons().add(new Image(App.class.getResourceAsStream("icons/icon.png")));
         stage.setScene(scene);
         stage.setTitle("Paint");
         stage.setResizable(false);
         stage.show();
     }
-
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
