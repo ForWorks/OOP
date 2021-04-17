fd1e730708cacd618d73ea977447f0406dd40f93
ForWorks <vladikgatilo@gmail.com>
2021-04-17 18:12:08 +0300

    fourth version

--- src/main/java/org/example/Shapes/Shape.java
+++ src/main/java/org/example/Shapes/Shape.java
@@ -2,16 +2,15 @@ package org.example.Shapes;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class Shapes {
-    protected Color penColor;
-    protected Color brushColor;
+public class Shape {
     protected double lineWeight;
+    protected Color brushColor;
+    protected Color penColor;
     protected List<Double> points = new ArrayList<>();
-    public void draw(GraphicsContext gc) {
+    public  void draw(GraphicsContext gc) {
 
-    }
+    };
 }
